605ba056c12363b0b40e3f67cf66fda5e3339270
Autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-06-28 17:55:20 -0400

    Update exit.asm

--- src/init/exit.asm
+++ src/init/exit.asm
@@ -1,18 +1,8 @@
 ; =============================================================================
-; MODULE: EXIT (Init / Ejecutor de Entorno Inicial)
+; MODULE: EXIT (Init)
 ; =============================================================================
 
 global exit_main_executor
 exit_main_executor:
-    ; Sincronizar punteros base del sistema de archivos EXFS
     mov qword [exfs_cur_dir_lba], 38
-    
-    ; Inicializar buffer de entrada de comandos
-    mov rdi, readline_buf
-    xor eax, eax
-    mov ecx, 32
-    rep stosq
-
-    ; Ceder control a la shell
-    call xsh_interactive_loop
     ret
