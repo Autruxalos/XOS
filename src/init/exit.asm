179c49d63c7dc9e39a40b54330c9eebe6921ca5a
Autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-06-28 17:42:40 -0400

    Update exit.asm

--- src/init/exit.asm
+++ src/init/exit.asm
@@ -1,23 +1,18 @@
 ; =============================================================================
 ; MODULE: EXIT (Init / Ejecutor de Entorno Inicial)
 ; =============================================================================
-[BITS 64]
 
+global exit_main_executor
 exit_main_executor:
-    ; 1. Sincronizar nombres y asegurar que estamos apuntando al LBA raíz
-    mov qword [exfs_cur_dir_lba], 38 ; EXFS_DATA_LBA definido en tu data
+    ; Sincronizar punteros base del sistema de archivos EXFS
+    mov qword [exfs_cur_dir_lba], 38
     
-    ; 2. Limpiar el buffer de comandos para evitar basura de memoria
+    ; Inicializar buffer de entrada de comandos
     mov rdi, readline_buf
     xor eax, eax
     mov ecx, 32
     rep stosq
 
-    ; 3. Transferir el control total a la Shell del sistema
+    ; Ceder control a la shell
     call xsh_interactive_loop
-
-    ; Si la shell retorna (por ejemplo, ejecutan 'halt'), EXIT toma el control del hardware
-    cli
-.system_deadlock:
-    hlt
-    jmp .system_deadlock
+    ret
