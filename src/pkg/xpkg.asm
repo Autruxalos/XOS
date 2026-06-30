30c5da06c12051bdc0d07b700590647e75a41bae
Autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-06-30 06:32:38 -0400

    Update xpkg.asm

--- src/pkg/xpkg.asm
+++ src/pkg/xpkg.asm
@@ -1,10 +1,10 @@
 ; XPKG - Instalador rápido
-xpkg_install:
+xpkg-install:
     ; Leer header del .xpkg
     ; Verificar magic "XPKG"
     ; Extraer XEXE y escribir en |packages| o |apps|
     ret
 
-xpkg_list:
+xpkg-list:
     ; Listar paquetes instalados en EXFS
     ret
