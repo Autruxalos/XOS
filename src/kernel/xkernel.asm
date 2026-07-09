1110bd7486ce3cafef7a7e1ce334bb55c9a3d9ee
Autruxalos <rivasfrismanabeleduardo@gmail.com>
2026-07-09 13:55:55 -0400

    Update xkernel.asm

--- src/kernel/xkernel.asm
+++ src/kernel/xkernel.asm
@@ -1,103 +1,80 @@
-org 0x0000
-bits 16
+org 0x9000
 
-fase_16_bits:
+[BITS 16]
+kernel_16_entry:
     cli
-    mov ax, 0x1000
+    xor ax, ax
     mov ds, ax
     mov es, ax
-    xor ax, ax
     mov ss, ax
     mov sp, 0x7C00
-
-    in al, 0x92
-    or al, 2
-    out 0x92, al
-
-    ; Ajuste de la GDT de 32 bits a dirección física lineal (0x10000)
-    mov eax, gdt32_start
-    add eax, 0x10000
-    mov [gdt32_descriptor_fisico + 2], eax
-    lgdt [gdt32_descriptor_fisico]
-
+    mov si, msg16
+    call print16
+    lgdt [gdt32]
     mov eax, cr0
     or eax, 1
     mov cr0, eax
+    jmp 0x08:kernel_32
 
-    jmp 0x08:(0x10000 + fase_32_bits)
+print16:
+.loop lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop
+.done ret
+msg16 db '16->',0
 
-bits 32
-fase_32_bits:
+[BITS 32]
+kernel_32:
     mov ax, 0x10
     mov ds, ax
-    mov es, ax
     mov ss, ax
+    mov esp, stack32
+    mov esi, msg32
+    call print32
+    lgdt [gdt64]
+    jmp 0x18:kernel_64
 
-    ; Paginación Directa (Identity Mapping de los primeros 2MB)
-    mov edi, 0x1000
-    mov cr3, edi
-    xor eax, eax
-    mov ecx, 3072
-    rep stosd
+print32:
+.loop lodsb
+    or al, al
+    jz .done
+    mov ah, 0x0E
+    int 0x10
+    jmp .loop
+.done ret
+msg32 db '32->',0
 
-    mov dword [0x1000], 0x2003  ; PML4 -> PDPT
-    mov dword [0x2000], 0x3003  ; PDPT -> PD
-    mov dword [0x3000], 0x0083  ; PD -> 2MB Gigante
-
-    mov eax, cr4
-    or eax, 1 << 5              ; Habilitar PAE
-    mov cr4, eax
-
-    mov ecx, 0xC0000080         ; EFER MSR
-    rdmsr
-    or eax, 1 << 8              ; LME = 1
-    wrmsr
-
-    mov eax, cr0
-    or eax, 1 << 31             ; Paging = 1
-    mov cr0, eax
-
-    ; Ajuste de la GDT de 64 bits a dirección física lineal (0x10000)
-    mov eax, gdt64_start
-    add eax, 0x10000
-    mov [gdt64_descriptor_fisico + 2], eax
-    lgdt [gdt64_descriptor_fisico]
-
-    jmp 0x08:(0x10000 + fase_64_bits)
-
-bits 64
-fase_64_bits:
-    xor ax, ax
+[BITS 64]
+kernel_64:
+    xor rax, rax
     mov ds, ax
-    mov es, ax
     mov ss, ax
+    mov rsp, stack64
 
-    ; Pintar "XOS" en pantalla
-    mov rax, 0x0F4F0F580F4F580F 
-    mov qword [0xB8000], rax
+    mov rdi, 0xB8000
+    mov rax, 0x1F581F4F
+    stosq
+    mov rax, 0x1F53201F
+    stosq
 
-.bucle_kernel:
+    call exit_main
+
+halt:
     hlt
-    jmp .bucle_kernel
+    jmp halt
 
-align 4
-gdt32_start:
-    dd 0, 0
-    dw 0xFFFF, 0x0000, 0x9A00, 0x00CF
-    dw 0xFFFF, 0x0000, 0x9200, 0x00CF
-gdt32_end:
+; GDTs
+gdt32 dw 23, gdt32_start, 0
+gdt32_start dq 0, 0xCF9A000000FFFF, 0xCF92000000FFFF
 
-gdt32_descriptor_fisico:
-    dw gdt32_end - gdt32_start - 1
-    dd 0
+gdt64 dw 15, gdt64_start, 0
+gdt64_start dq 0, 0x209A0000000000
 
-align 4
-gdt64_start:
-    dd 0, 0
-    dw 0, 0, 0x9A00, 0x0020
-    dw 0, 0, 0x9200, 0x0000
-gdt64_end:
+stack32 times 512 db 0
+stack64 times 1024 db 0
 
-gdt64_descriptor_fisico:
-    dw gdt64_end - gdt64_start - 1
-    dd 0
+%include "src/init/exit.asm"
+%include "src/apps/xsh.asm"
